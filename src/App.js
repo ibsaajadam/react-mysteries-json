448fec1bd1eb3f4bb65ce67ba5bd00ef2e4e4327
Ibsaa J. Adam <ibsaa.adam1@gmail.com>
2022-07-12 00:26:38 -0400

    added header/title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import SearchBar from "./Components/SearchBar";
+import Header from "./Components/Header";
 import BookData from "./Data.json";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <SearchBar placeholder="Search Mystery Movies..." data={BookData} />
     </div>
   );
